54b3be6cb8ce59fe985bce1cffd7a282337e004f
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-11-02 17:55:53 +0200

    Update app.js

--- Asynchronous Programming - Exercise/04.Locked-Profile/app.js
+++ Asynchronous Programming - Exercise/04.Locked-Profile/app.js
@@ -14,18 +14,6 @@ async function lockedProfile() {
   });
   mainSection.children[0].remove();
 
-  // fetch('http://localhost:3030/jsonstore/advanced/profiles')
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     let profileDiv = document.querySelector('.profile');
-
-  //     Object.values(data).forEach((person) => {
-  //       const profile = profileDiv.cloneNode(true);
-  //       mainSection.appendChild(editProfile(profile, person));
-  //     });
-  //     mainSection.children[0].remove();
-  //   });
-
   function editProfile(profile, data) {
     profile.querySelector('input[name="user1Username"]').value = data.username;
     profile.querySelector('input[name="user1Email"]').value = data.email;
@@ -56,3 +44,49 @@ async function lockedProfile() {
     }
   }
 }
+
+// function lockedProfile() {
+//   let mainSection = document.querySelector('#main');
+
+//   fetch('http://localhost:3030/jsonstore/advanced/profiles')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let profileDiv = document.querySelector('.profile');
+
+//       Object.values(data).forEach((person) => {
+//         const profile = profileDiv.cloneNode(true);
+//         mainSection.appendChild(editProfile(profile, person));
+//       });
+//       mainSection.children[0].remove();
+//     });
+
+//   function editProfile(profile, data) {
+//     profile.querySelector('input[name="user1Username"]').value = data.username;
+//     profile.querySelector('input[name="user1Email"]').value = data.email;
+//     profile.querySelector('input[name="user1Age"]').value = data.age;
+//     profile.querySelector('input[name="user1Age"]').type = 'email';
+
+//     const div = profile.getElementsByClassName('user1Username')[0];
+//     div.className = '';
+//     div.id = 'user1HiddenFields';
+//     div.style.display = 'none';
+
+//     profile.querySelector('button').addEventListener('click', showMore);
+//     return profile;
+//   }
+
+//   function showMore(e) {
+//     const profile = e.currentTarget.parentNode;
+//     const unlock = profile.querySelector('input[value="unlock"]').checked;
+
+//     if (unlock) {
+//       if (e.currentTarget.textContent == 'Show more') {
+//         profile.querySelector('#user1HiddenFields').style.display = 'block';
+//         e.currentTarget.textContent = 'Hide it';
+//       } else {
+//         profile.querySelector('#user1HiddenFields').style.display = 'none';
+//         e.currentTarget.textContent = 'Show more';
+//       }
+//     }
+//   }
+// }
