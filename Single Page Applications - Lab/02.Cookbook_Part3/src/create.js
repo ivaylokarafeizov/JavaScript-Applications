284c2c13f5b6436a5c284d41fabf4a02e0fdb7d7
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-11-08 16:42:08 +0200

    Add 02.Cookbook_Part3

---
+++ Single Page Applications - Lab/02.Cookbook_Part3/src/create.js
@@ -0,0 +1,68 @@
+import { showDetails } from './details.js';
+
+let main;
+let section;
+let setActiveNav;
+
+export function setupCreate(targetMain, targetSection, onActiveNav) {
+  main = targetMain;
+  section = targetSection;
+  setActiveNav = onActiveNav;
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const formData = new FormData(ev.target);
+    onSubmit(
+      [...formData.entries()].reduce(
+        (p, [k, v]) => Object.assign(p, { [k]: v }),
+        {}
+      )
+    );
+  });
+
+  async function onSubmit(data) {
+    const body = JSON.stringify({
+      name: data.name,
+      img: data.img,
+      ingredients: data.ingredients
+        .split('\n')
+        .map((l) => l.trim())
+        .filter((l) => l != ''),
+      steps: data.steps
+        .split('\n')
+        .map((l) => l.trim())
+        .filter((l) => l != ''),
+    });
+
+    const token = sessionStorage.getItem('authToken');
+    if (token == null) {
+      return (window.location.pathname = 'index.html');
+    }
+
+    try {
+      const response = await fetch('http://localhost:3030/data/recipes', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Authorization': token,
+        },
+        body,
+      });
+
+      if (response.status == 200) {
+        showDetails((await response.json())._id);
+      } else {
+        throw new Error(await response.json());
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+}
+
+export function showCreate() {
+  setActiveNav('createLink');
+  main.innerHTML = '';
+  main.appendChild(section);
+}
