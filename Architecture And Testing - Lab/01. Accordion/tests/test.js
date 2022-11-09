436995c04064e4cf34519b94b6bd104aab2f0c25
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-11-09 13:16:34 +0200

    Update test.js

--- Architecture And Testing - Lab/01. Accordion/tests/test.js
+++ Architecture And Testing - Lab/01. Accordion/tests/test.js
@@ -22,34 +22,39 @@ describe('E2E tests', function () {
     await page.close();
   });
 
-  it('load static page', async () => {
-    await page.goto('http://127.0.0.1:5500/index.html');
+  it('loads titles', async () => {
+    await page.goto('http://localhost:5500/');
+    await page.waitForSelector('.accordion .head span');
 
-    const content = await page.textContent('.accordion .head span');
+    const content = await page.textContent('#main');
+
     expect(content).to.contains('Scalable Vector Graphics');
+    expect(content).to.contains('Open standard');
+    expect(content).to.contains('Unix');
+    expect(content).to.contains('ALGOL');
   });
 
-  it('toggles content', async () => {
-    await page.goto('http://127.0.0.1:5500/index.html');
+  it('button “More” functionality', async () => {
+    await page.goto('http://localhost:5500/');
 
-    await page.click('#main>.accordion:first-child >> text=More');
-    await page.waitForSelector('#main>.accordion:first-child >> .extra p');
-    const visible = await page.isVisible(
-      '#main>.accordion:first-child >> .extra p'
+    await page.click('text=More');
+    await page.waitForSelector('.extra p');
+
+    const text = await page.textContent('.extra p');
+
+    expect(text).to.be.contain(
+      'Scalable Vector Graphics (SVG) is an Extensible Markup Language'
     );
-    expect(visible).to.be.true;
   });
 
-  it('toggles content', async () => {
-    await page.goto('http://127.0.0.1:5500/index.html');
+  it('button “Less” functionality', async () => {
+    await page.goto('http://localhost:5500/');
+    await page.click('text=More');
+    await page.waitForSelector('.extra p');
+    await page.click('text=Less');
 
-    await page.click('#main>.accordion:first-child >> text=More');
-    await page.waitForSelector('#main>.accordion:first-child >> .extra p');
-    await page.click('#main>.accordion:first-child >> text=Less');
+    const visible = await page.isVisible('.extra p');
 
-    const visible = await page.isVisible(
-      '#main>.accordion:first-child >> .extra p'
-    );
     expect(visible).to.be.false;
   });
 });
