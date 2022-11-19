b8f6f0d8696fd264b6949a5dd76a90ea94782190
Ivaylo Karafeizov <qwerty36758@gmail.com>
2022-11-19 19:46:12 +0200

    Update app.js

--- Asynchronous Programming - Exercise/06.Blog/app.js
+++ Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,56 +1,56 @@
-function attachEvents() {
-  let select = document.querySelector('#posts');
-  let btnLoadPosts = document.querySelector('#btnLoadPosts');
-  let btnViewPost = document.querySelector('#btnViewPost');
-  let title = document.getElementById('post-title');
-  let body = document.getElementById('post-body');
-  let postComments = document.getElementById('post-comments');
+const baseUrl = 'http://localhost:3030/jsonstore/blog';
+const select = document.getElementById('posts');
+const btnViewPosts = document.getElementById('btnViewPost');
+const title = document.getElementById('post-title');
+const body = document.getElementById('post-body');
+const postComments = document.getElementById('post-comments');
 
-  btnLoadPosts.addEventListener('click', loadPosts);
+const loadPostsBtn = document.getElementById('btnLoadPosts');
+loadPostsBtn.addEventListener('click', loadPosts);
 
-  function loadPosts() {
-    fetch('http://localhost:3030/jsonstore/blog/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        select.innerHTML = '';
-        Object.values(data).forEach((post) => {
-          let option = document.createElement('option');
-          option.value = post.id;
-          option.textContent = post.title;
+async function loadPosts() {
+  try {
+    const response = await fetch(baseUrl + '/posts');
+    const data = await response.json();
 
-          select.appendChild(option);
-        });
-        btnViewPost.addEventListener('click', () => viewPosts(data));
-      })
-      .catch((error) => console.log(error.message));
-  }
+    select.innerHTML = '';
+    for (const key in data) {
+      const option = document.createElement('option');
+      option.value = key;
+      option.textContent = data[key].title;
+      select.appendChild(option);
+    }
 
-  function viewPosts(dataSelect) {
-    fetch('http://localhost:3030/jsonstore/blog/comments')
-      .then((response) => response.json())
-      .then((data) => {
-        let comments = Object.values(data).filter(
-          (obj) => select.value == obj.postId
-        );
-
-        let selectedOption = [...select.options].find(
-          (o) => o.value == select.value
-        );
-
-        title.textContent = selectedOption.textContent;
-        body.textContent = dataSelect[select.value].body;
-
-        postComments.innerHTML = '';
-        comments.forEach((comment) => {
-          let li = document.createElement('li');
-          li.id = comment.id;
-          li.textContent = comment.text;
-
-          postComments.appendChild(li);
-        });
-      })
-      .catch((error) => console.log(error.message));
+    btnViewPosts.addEventListener('click', () => viewPosts(data));
+  } catch (error) {
+    console.log(error.message);
   }
 }
 
-attachEvents();
+async function viewPosts(dataSelect) {
+  try {
+    const response = await fetch(baseUrl + '/comments');
+    const data = await response.json();
+
+    const comments = Object.values(data).filter(
+      (obj) => select.value === obj.postId
+    );
+
+    const selectedOption = [...select.options].find(
+      (o) => o.value === select.value
+    );
+
+    title.textContent = selectedOption.textContent;
+    body.textContent = dataSelect[select.value].body;
+
+    postComments.innerHTML = '';
+    comments.forEach((comment) => {
+      const li = document.createElement('li');
+      li.id = comment.id;
+      li.textContent = comment.text;
+      postComments.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
